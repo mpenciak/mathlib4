84afd739bdd234749ba033d9657fda22d927ea70
Kim Morrison <kim@tqft.net>
2026-02-19 10:54:51 +1100

    fixes for leanprover/lean4#12564

--- Mathlib/RingTheory/MvPolynomial.lean
+++ Mathlib/RingTheory/MvPolynomial.lean
@@ -52,6 +52,10 @@ set_option backward.isDefEq.respectTransparency false in
 theorem rank_eq {σ : Type v} : Module.rank K (MvPolynomial σ K) = #(σ →₀ ℕ) := by
   rw [← Cardinal.lift_inj, ← (basisMonomials σ K).mk_eq_rank]
 
+#adaptation_note /-- Needed after leanprover/lean4#12564 -/
+instance : Module K (MvPolynomial σ K) :=
+  inferInstanceAs <| Module K (AddMonoidAlgebra K (σ →₀ ℕ))
+
 theorem finrank_eq_zero [Nonempty σ] : Module.finrank K (MvPolynomial σ K) = 0 :=
   (basisMonomials σ K).linearIndependent.finrank_eq_zero_of_infinite
 
